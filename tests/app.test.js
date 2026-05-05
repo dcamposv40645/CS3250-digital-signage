24c98e10e970556ff8ce257da33b8c37cbf87602
dcamposv40645 <dcamposv@msudenver.edu>
2026-05-05 01:57:45 -0600

    test: add more escapeHtml tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,21 @@
 const { escapeHtml, getWeatherSymbol, getValuePath, stripHtml, parseConfig, buildRssHtml } = require('../app.module');
 describe('escapeHtml', () => {
     test('escapes ampersands', () => {
-      expect(escapeHtml('cats & dogs')).toBe('cats &amp; dogs');
+        expect(escapeHtml('cats & dogs')).toBe('cats &amp; dogs');
     });
-  });
+    test('escapes < and >', () => {
+        expect(escapeHtml('<script>')).toBe('&lt;script&gt;');
+    });
+
+    test('escapes double quotes', () => {
+        expect(escapeHtml('"hello"')).toBe('&quot;hello&quot;');
+    });
+
+    test('escapes single quotes', () => {
+        expect(escapeHtml("it's")).toBe('it&#39;s');
+    });
+
+    test('returns plain text unchanged', () => {
+        expect(escapeHtml('hello world')).toBe('hello world');
+    });
+});
